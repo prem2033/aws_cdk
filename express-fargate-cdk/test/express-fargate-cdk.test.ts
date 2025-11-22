9598678a46a4dc246c39a212bda6fc0d7016c1bc
Prem Prakash <42450106+prem2033@users.noreply.github.com>
2025-11-22 20:22:30 +0000

    ci: ts app for dec deployment for ecs

---
+++ express-fargate-cdk/test/express-fargate-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ExpressFargateCdk from '../lib/express-fargate-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/express-fargate-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ExpressFargateCdk.ExpressFargateCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
